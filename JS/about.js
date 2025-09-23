dfcd7eda513935d47887eb7268f6e21f1601aab8
Akash Agasur <agasurakash@gmail.com>
2025-09-23 20:50:35 +0530

    work and about page updated

--- JS/about.js
+++ JS/about.js
@@ -8,3 +8,33 @@ document.body.addEventListener("mousemove", (e) => {
 
   magnet.style.transform = `translate(${x}px, ${y}px) translate(-50%, -50%)`;
 });
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+// Animate the hero text section
+gsap.from(".hero .left", {
+  scrollTrigger: {
+    trigger: ".hero .left",
+    start: "top 80%",
+    toggleActions: "play none none none",
+  },
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  ease: "power3.out",
+});
+
+// Animate social media buttons with stagger
+gsap.from(".social-media button", {
+  scrollTrigger: {
+    trigger: ".social-media",
+    start: "top 90%",
+    toggleActions: "play none none none",
+  },
+  y: 30,
+  opacity: 0,
+  duration: 1,
+  ease: "power2.out",
+  stagger: 0.2,
+});
