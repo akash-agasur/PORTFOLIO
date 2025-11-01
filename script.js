e4010b8b7b6db9233f429b484f58bf5f3f803ab9
Akash Agasur <agasurakash@gmail.com>
2025-11-01 05:47:34 +0530

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+let ngtmode = document.querySelector(".ngt-mode");
+let main = document.querySelector(".main");
+let body = document.querySelector("body");
+
+
+
+ngtmode.addEventListener("click", () => {
+    console.log("hello");
+body.style.backgroundColor = "#1C1C2B";
+main.style.backgroundColor = "#1C1C2B";
+main.style.Color = "white";
+
+});
